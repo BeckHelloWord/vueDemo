158fd5df7d3ef55c544dbce217791e2f971c4b4b
BeckHelloWord <444870506@qq.com>
2018-12-23 16:06:27 +0800

    add normalize.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import "../src/assets/icons/iconfont.css";
+import "normalize.css";
 
 Vue.config.productionTip = false;
 
